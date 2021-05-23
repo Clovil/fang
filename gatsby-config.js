3b67d65b3df3ef2903831b8e071607725177ddc2
raymondh <raymondh@tophatter.com>
2021-05-23 20:26:23 +0800

    Revert "Update Blog “2020-11-22-html-css-javascript”"
    This reverts commit c19f39c06fc2aa960afd2d8b7e50958ae8e0f3fd.

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-netlify-cms',
+      resolve: `gatsby-plugin-netlify-cms`,
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
